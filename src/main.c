778c0acb6b2121625b8bfb1c4abf0695299c70b8
Rhipheus <rhipheus114@gmail.com>
2023-10-01 00:57:43 +0300

    screen added

--- src/main.c
+++ src/main.c
@@ -8,18 +8,7 @@ Chip8* chip8_ptr = &chip8;
 
 int main(int argv, char** args)
 {
-    //loadFont(&chip8);
-    loadRom(chip8_ptr,"test/slipperyslope.ch8");
 
-    // returns zero on success else non-zero
-    if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
-        printf("error initializing SDL: %s\n", SDL_GetError());
-    }
-    SDL_Window* win = SDL_CreateWindow("GAME",
-                                       SDL_WINDOWPOS_CENTERED,
-                                       SDL_WINDOWPOS_CENTERED,
-                                       1000, 1000, 0);
-    while (1)
-        ;
-    return 0;
+    loadRom(chip8_ptr,"test/slipperyslope.ch8");
+   
 }
---
+++ src/screen.h
@@ -0,0 +1,9 @@
+#ifndef SCREEN_H
+#define SCREEN_H
+
+#include "chip8.h"
+
+int initWindow(Chip8* chip8);
+int initWindow(Chip8* chip8);
+
+#endif
